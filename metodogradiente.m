c8b5702df28248e610724fe243759ac538f7a767
Jesica Maia Numerosky <jnumerosky@ws6.labo3>
2017-05-12 18:54:57 -0300

    fminbnd

--- metodogradiente.m
+++ metodogradiente.m
@@ -1,10 +1,10 @@
 clear all, close all
 f = @(x) (1-x(2)).^2 + 100*(x(1)-x(2).^2).^2;
 %grad = @(x) [200*(x(1)-x(2).^2), 2*x(2)-2-400*(x(1)-x(2).^2).*x(2)];
-grad = @(x) gradiente(f,x)';
+grad = @(x) gradiente(f,x);
 %f = @(x) (x(1)-x(2)).^4+2*x(1).^2+x(2).^2-x(1)+2*x(2);
 %grad = @(x) [4*(x(1)-x(2)).^3+4*x(1)-1 , -4*(x(1)-x(2)).^3+2*x(2)+2];
-x = [-0.8,-1];
+x = [0;0];
 puntox = x(1);
 puntoy = x(2);
 n = 1;
@@ -18,10 +18,10 @@ shading('INTERP')
 
 hold on
 
-while norm(grad(x)) > 0.001 && n < 10000
-    d = -grad(x);
+while norm(grad(x)) > 0.001 && n < 1000
+    d = -grad(x)';
     phi = @(t) f(x + t*d);
-    t = fminsearch(phi,0);
+    t = fminbnd(phi,0,10);
     x = x + t * d;
     %puntox = [puntox x(1)];
     %puntoy = [puntoy x(2)];
@@ -30,5 +30,3 @@ while norm(grad(x)) > 0.001 && n < 10000
 end
 
 %plot3(puntox, puntoy,f(punto))
-
-
