56d1553eea0fba52e4e537c8ceed96801987820d
Jesica Maia Numerosky <jnumerosky@ws4.labo2>
2017-05-26 15:32:52 -0300

    archivo resultados

--- armijo.m
+++ armijo.m
@@ -1,12 +1,12 @@
 function t = armijo(f,x,eta)
 
-d = -grad(x)';
+d = (-1)*Grad(x)';
 t = 1;
 
-while f(x + t*dk') > f(x) + t*0.2*grad(x)*d
+while f(x + t*d) > f(x) + t*0.2*Grad(x)*d
   t = t/eta;
 end
 
-while f(x + eta*t*dk') <= f(x) + eta*t*0.2*grad(x)*d
+while f(x + eta*t*d) <= f(x) + eta*t*0.2*Grad(x)*d
     t = eta * t;
 end
